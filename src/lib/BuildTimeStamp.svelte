72d0e2c8f63ac293f8ffc94dba17bc7e6da17b88
mnyrop <marii@nyu.edu>
2021-11-11 17:17:45 -0500

    append iframe

--- src/lib/BuildTimeStamp.svelte
+++ src/lib/BuildTimeStamp.svelte
@@ -1,1 +1,1 @@
-<script context='module'>export const timestamp ='11/11/21 @ 04:27 EST';</script>
+<script context='module'>export const timestamp ='11/11/21 @ 04:39 EST';</script>
--- src/lib/QueryBuilder.svelte
+++ src/lib/QueryBuilder.svelte
@@ -4,8 +4,7 @@
 
 	let placeholderText = 'URI input';
 	let visibleValidCheck = false;
-	let manifestURI =
-		'https://iiif.bodleian.ox.ac.uk/iiif/manifest/e32a277e-91e2-4a6d-8ba6-cc4bad230410.json';
+	let manifestURI = 'https://iiif.bodleian.ox.ac.uk/iiif/manifest/e32a277e-91e2-4a6d-8ba6-cc4bad230410.json';
 	let viewers = [
 		{
 			label: 'Mirador',
@@ -20,6 +19,7 @@
 	let themes = ['Light', 'Dark'];
 	let selectedTheme = themes[0];
 	let query = buildQuery();
+	let iframeContainer;
 
 	function validateManifest() {
 		visibleValidCheck = !visibleValidCheck;
@@ -32,70 +32,77 @@
 		}/viewer.html${hotfix}?manifest=${manifestURI}?theme=${selectedTheme.toLowerCase()}`;
 	}
 
-	function handleSubmit() {
-		query = buildQuery();
-		window.open(query, '_blank');
-	}
-
 	function clearManifest() {
 		manifestURI = '';
 	}
+
+	function validateAndSubmit() {
+		query = buildQuery();
+		let existing = document.getElementById('viewer-iframe');
+		if (existing !== null) { existing.remove(); }
+
+		let iframe = document.createElement('iframe');
+    iframe.setAttribute('src', query);
+		iframe.setAttribute('id', 'viewer-iframe');
+		iframe.style.width = "100%";
+		iframe.style.height = "500px";
+		iframeContainer.appendChild(iframe);
+	}
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-	<div class="field">
-		<label class="label">Manifest</label>
-		<div class="control">
-			<input bind:value={manifestURI} class="input" type="text" placeholder={placeholderText} />
+<div class="field">
+	<label class="label">Manifest</label>
+	<div class="control">
+		<input bind:value={manifestURI} class="input" type="text" placeholder={placeholderText} />
+	</div>
+</div>
+
+<div class="field">
+	<div class="control pt-3 pb-5">
+		<button on:click={clearManifest} class="button is-link">Clear</button>
+		<button on:click={validateManifest} class="button is-link">Validate
+			{#if visibleValidCheck}
+				<span in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>&nbsp&nbsp;✅</span>
+			{/if}
+		</button>
+	</div>
+</div>
+
+<div class="field">
+	<label class="label">Viewer</label>
+	<div class="control">
+		<div class="select">
+			<select bind:value={selectedViewer}>
+				{#each viewers as viewer}
+					<option value={viewer}>{viewer.label}</option>
+				{/each}
+			</select>
 		</div>
 	</div>
+</div>
 
-	<div class="field">
-		<div class="control pt-3 pb-5">
-			<button on:click={clearManifest} class="button is-link">Clear</button>
-			<button on:click={validateManifest} class="button is-link"
-				>Validate{#if visibleValidCheck}<span
-						in:fade={{ duration: 200 }}
-						out:fade={{ duration: 200 }}>&nbsp&nbsp;✅</span
-					>{/if}</button
-			>
+<div class="field">
+	<label class="label">Theme</label>
+	<div class="control">
+		<div class="select">
+			<select bind:value={selectedTheme}>
+				{#each themes as theme}
+					<option value={theme}>{theme}</option>
+				{/each}
+			</select>
 		</div>
 	</div>
+</div>
 
-	<div class="field">
-		<label class="label">Viewer</label>
-		<div class="control">
-			<div class="select">
-				<select bind:value={selectedViewer}>
-					{#each viewers as viewer}
-						<option value={viewer}>{viewer.label}</option>
-					{/each}
-				</select>
-			</div>
-		</div>
-	</div>
-
-	<div class="field">
-		<label class="label">Theme</label>
-		<div class="control">
-			<div class="select">
-				<select bind:value={selectedTheme}>
-					{#each themes as theme}
-						<option value={theme}>{theme}</option>
-					{/each}
-				</select>
-			</div>
-		</div>
-	</div>
-
+<form on:submit|preventDefault={validateAndSubmit}>
 	<div class="field">
 		<div class="control pt-3">
-			<button type="submit" class="button is-link">Submit</button>
+			<button disabled={!manifestURI} type="submit" class="button is-link">Submit</button>
 		</div>
 	</div>
 </form>
 
-<div class="content mt-5">
+<div class="content mt-5" bind:this={iframeContainer}>
 	<h3>Your Query</h3>
 	<p><a href={query} target="_blank">{query}</a></p>
 </div>
