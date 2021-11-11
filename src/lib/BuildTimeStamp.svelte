d1e83c670ac11394fe29a4b607ddd02c230b16b7
mnyrop <marii@nyu.edu>
2021-11-11 16:29:46 -0500

    build timestamp

---
+++ src/lib/BuildTimeStamp.svelte
@@ -0,0 +1,1 @@
+<script context='module'>export const timestamp ='11/11/21 @ 04:27 EST';</script>
--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -1,11 +1,12 @@
 <script>
 	import { base } from '$app/paths';
+	import { timestamp } from '$lib/BuildTimeStamp.svelte';
+
 	export let siteTitle, siteDescription;
-	export let buildTime = new Date().toLocaleString();
 </script>
 
 <h1 class="title">
 	<a href="{base}/">{siteTitle} </a>
 </h1>
 <p class="subtitle">{siteDescription}</p>
-<p class="pb-5">last deployed @ {buildTime.toLowerCase()}</p>
+<p class="pb-5">last built on {timestamp.toLowerCase()}</p>
