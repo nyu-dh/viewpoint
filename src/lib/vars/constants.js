19a95f7184ffd7a7dac41578fa5b20a822887156
marii¨ <mnyrop@users.noreply.github.com>
2023-02-16 15:47:24 -0500

    rm broken manifests

--- src/lib/vars/constants.js
+++ src/lib/vars/constants.js
@@ -4,7 +4,6 @@ export const sampleManifests = [
 	'https://purl.stanford.edu/wr796rv9498/iiif/manifest',
 	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b90266728/manifest.json',
 	'https://iiif.bodleian.ox.ac.uk/iiif/manifest/e32a277e-91e2-4a6d-8ba6-cc4bad230410.json',
-	'https://jsonstorage.net/api/items/1a6e2d6e-fa90-4d44-95a5-35bb6c011aa2',
 	'https://www.e-codices.unifr.ch/metadata/iiif/bc-s-0007/manifest.json',
 	'https://www.e-codices.unifr.ch/metadata/iiif/bc-s-0058/manifest.json',
 	'https://www.e-codices.unifr.ch/metadata/iiif/snm-AG002760/manifest.json',
@@ -12,8 +11,7 @@ export const sampleManifests = [
 	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b53212522c/manifest.json',
 	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b8400342f/manifest.json',
 	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b84006642/manifest.json',
-	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b8490923n/manifest.json',
-	'https://jsonstorage.net/api/items/ce49ca3a-3031-4914-bb60-069a65642f9f'
+	'https://gallica.bnf.fr/iiif/ark:/12148/btv1b8490923n/manifest.json'
 ];
 export const staticPath = import.meta.env.DEV ?  '' : '/viewpoint';
 export const placeholderText = 'URI input';
